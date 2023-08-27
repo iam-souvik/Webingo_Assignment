793e2e18925201a4ed653c9e4b0d1b01cdf39df2
iam-souvik <souvikpatramax@gmail.com>
2023-08-27 13:08:44 +0530

    final_cimmit

--- frontend/src/components/AdminPanel.jsx
+++ frontend/src/components/AdminPanel.jsx
@@ -150,6 +150,7 @@ const AdminPanel = () => {
                     isClosable: true,
                     position: "top"
                })
+               
           } finally {
                setLoading(false)
                event.target.reset();
